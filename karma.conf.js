9b12acb18dadacb4e0ddabbfd1c53379d0068c1c
Robert C. Langefeld <rlangefe@umich.edu>
2024-07-25 10:30:12 -0400

    Update with real fixed

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
 			suppressAll: true // removes the duplicated traces
 		},
 		coverageReporter: {
-			dir: require('path').join(__dirname, './coverage/JayantGoel001'),
+			dir: require('path').join(__dirname, './coverage/rlangefe'),
 			subdir: '.',
 			reporters: [
 				{type: 'html'},
